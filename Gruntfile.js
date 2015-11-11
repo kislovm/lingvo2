bbe303b4cf69f1a4ebf172abf49b9d23a2470079
Maxim Kislov <kislovm@gmail.com>
2015-11-12 02:17:51 +0300

    menu in header

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
         watch: {
             scripts: {
                 files: ['client/templates/*.hbs', 'client/src/**/*.js', 'client/styles/*.css'],
-                tasks: ['clean:dev', 'browserify:app', 'concat', 'copy:dev']
+                tasks: ['clean:dev', 'browserify:app', 'concat','cssmin', 'copy:dev']
 
             },
             options: {
