a2bd2e875c1af6355c31544fd0875677cef2eccf
Maxim Kislov <kislovm@gmail.com>
2016-04-12 00:00:56 +0300

    new mail handler

--- email/controller.js
+++ email/controller.js
@@ -1,15 +1,29 @@
-var Email = require('email').Email;
+var Mail = require('mail').Mail;
 
-var senderMail = 'noreply@upvoc.com';
+var SMTPHandler = function() {
+    this.mailer = Mail({
+        host: 'smtp.yandex.ru',
+        username: 'admin@upvoc.com',
+        password: '1jklh2',
+        port: 465
+    });
+
+    this.senderEmail = 'noreply@upvoc.com';
+
+    this.send = function(toEmail, message) {
+        this.mailer.message({
+            from: this.senderEmail,
+            to: [toEmail],
+            subject: 'Upvoc.com registration'
+        })
+        .body(message)
+        .send();
+    };
+};
+
 
 var sendRegistrationEmail = function(email) {
-    new Email({
-        from: senderMail,
-        to: email,
-        subject: 'Upvoc.com registration',
-        body: 'Hi! You just registered on http://upvoc.com. Thank you.'
-    })
-    .send();
+    new SMTPHandler().send(email, 'Hi! You just registered on http://upvoc.com. Thank you.');
 };
 
 module.exports = {
