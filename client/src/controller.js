4c8e8da6b449f4f684e9f36a56ae41363b498961
Maxim Kislov <kislovm@gmail.com>
2016-04-23 01:18:59 +0300

    Moved UI to separate controller

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,86 +1,16 @@
 var Marionette = require('backbone.marionette');
-var DictionarySection = require('./views/dictionary-section');
-var LoginForm = require('./views/login');
-var Settings = require('./views/settings');
-var MobileFooter = require('./views/mobile-footer');
-var AppLayoutView = require('./views/layouts/app-layout');
+
+var UiController = require('./ui-controller');
+
 var EpisodesView = require('./views/episodes');
 var EpisodesCollection = require('./collections/episodes');
-var TipOfTheDayView = require('./views/tip-of-the-day');
-var MenuView = require('./views/menu');
-var LanguageView = require('./views/language');
-var $ = require('jquery');
+
 
 module.exports = Marionette.Controller.extend({
     initialize: function() {
         App.core.vent.trigger('app:log', 'Controller: Initializing');
-        App.layoutView = new AppLayoutView();
 
-        App.core.vent.bind('router:inited', function() {
-            App.menuView = new MenuView({ el: $('.menu' )});
-        }, this);
-
-        App.core.vent.bind('show-login', function() {
-            App.layoutView.showChildView('dictionaries', new LoginForm());
-        }, this);
-
-        App.core.vent.bind('dictionary:show', this.onShowDictionary, this);
-        App.core.vent.bind('dictionary:close', this.onCloseDictionary, this);
-
-        $(window).scroll(function() {
-            clearTimeout($.data(this, 'scrollTimer'));
-            $.data(this, 'scrollTimer', setTimeout(function() {
-                if($(window).scrollTop() >= $(document).height() - ($(window).height() * 2)) {
-                    App.layoutView.content.currentView.increment()
-                }
-            }, 50));
-        });
-
-        var centerBlock = $('.center-block');
-
-        centerBlock.scroll(function() {
-            clearTimeout($.data(this, 'scrollTimer'));
-            $.data(this, 'scrollTimer', setTimeout(function() {
-                if(centerBlock.scrollTop() >= $(document).height() - (centerBlock.height() * 2)) {
-                    App.layoutView.content.currentView.increment()
-                }
-            }, 50));
-        });
-
-        var onOrientationChange = function() {
-          if(window.matchMedia("(orientation: portrait)").matches) // Portrait
-          {
-            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=.75, maximum-scale=.75" />');
-          } else // Landscape
-          {
-            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />');
-          }
-        };
-
-        onOrientationChange();
-
-        $(window).on('orientationchange', onOrientationChange);
-
-        App.languageView = new LanguageView({ el: $('.lang-select'), model: App.data.user });
-
-        if(App.data.user.get('registred') === true) {
-            App.layoutView.showChildView('dictionaries', new DictionarySection());
-            App.layoutView.showChildView('settings', new Settings());
-        } else {
-            App.layoutView.showChildView('dictionaries', new LoginForm());
-        }
-
-        App.layoutView.showChildView('mobileFooter', new MobileFooter());
-
-        App.tipOfTheDay = new TipOfTheDayView({ el: $('.tip-of-the-day') });
-    },
-
-    onCloseDictionary: function() {
-        $('.dictionaries-section').removeClass('show');
-    },
-
-    onShowDictionary: function() {
-        $('.dictionaries-section').addClass('show').appendTo('body');
+        App.uiController = new UiController();
     },
 
     home: function() {
---
+++ client/src/ui-controller.js
@@ -0,0 +1,77 @@
+var Marionette = require('backbone.marionette');
+
+var $ = require('jquery');
+
+var AppLayoutView = require('./views/layouts/app-layout');
+
+var MenuView = require('./views/menu');
+var LanguageView = require('./views/language');
+var TipOfTheDayView = require('./views/tip-of-the-day');
+var LoginFormView = require('./views/login');
+var MobileFooterView = require('./views/mobile-footer');
+
+var DictionarySectionView = require('./views/dictionary-section');
+var SettingsView = require('./views/settings');
+
+module.exports = Marionette.Controller.extend({
+    initialize: function() {
+        this.listenTo(App.core.vent, 'router:inited', this.onRouterInited);
+        this.listenTo(App.core.vent, 'show-login', this.onShowLogin);
+
+        this.listenTo(App.core.vent, 'dictionary:show', this.onShowDictionary);
+        this.listenTo(App.core.vent, 'dictionary:close', this.onCloseDictionary);
+
+        //init page layout
+        App.layoutView = new AppLayoutView();
+        App.layoutView.showChildView('mobileFooter', new MobileFooterView());
+        App.tipOfTheDay = new TipOfTheDayView({ el: $('.tip-of-the-day') });
+        App.languageView = new LanguageView({ el: $('.lang-select'), model: App.data.user });
+
+        this.onOrientationChange();
+        $(window).on('orientationchange', this.onOrientationChange);
+
+        if(App.data.user.get('registred') === true) {
+            App.layoutView.showChildView('dictionaries', new DictionarySectionView());
+            App.layoutView.showChildView('settings', new SettingsView());
+        } else {
+            App.layoutView.showChildView('dictionaries', new LoginFormView());
+        }
+
+        $(window).scroll(this.onWindowScroll);
+    },
+
+    onRouterInited: function() {
+        App.menuView = new MenuView({ el: $('.menu' )});
+    },
+
+    onShowLogin: function() {
+        App.layoutView.showChildView('dictionaries', new LoginFormView());
+    },
+
+    onOrientationChange: function() {
+        if(window.matchMedia("(orientation: portrait)").matches) {
+            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=.75, maximum-scale=.75" />');
+        } else {
+            $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />');
+        }
+    },
+
+    onWindowScroll: function() {
+        clearTimeout($.data(this, 'scrollTimer'));
+        $.data(this, 'scrollTimer', setTimeout(function() {
+            if($(window).scrollTop() >= $(document).height() - ($(window).height() * 2)) {
+                App.layoutView.content.currentView.increment()
+            }
+        }, 50));
+    },
+
+    onCloseDictionary: function() {
+        $('.dictionaries-section').removeClass('show');
+        $('body').removeClass('body-fixed');
+    },
+
+    onShowDictionary: function() {
+        $('.dictionaries-section').addClass('show').appendTo('body');
+        $('body').removeClass('body-fixed');
+    }
+});
