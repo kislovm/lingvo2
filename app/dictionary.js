3c21e9c8706fcfdcecdf23747e7c6245ac768da3
Maxim Kislov <kislovm@gmail.com>
2016-04-23 17:06:16 +0300

    Deleted dicts

--- app/dictionary.js
+++ app/dictionary.js
@@ -1,31 +1,31 @@
-var models = require('../app/models');
-var langs = {
-    chinese: require('../chinese'),
-    arabic: require('../arabic'),
-    portugal: require('../portugal'),
-    russian: require('../russian'),
-    spanish: require('../spanish'),
-    turkish: require('../turkish')
-};
-
-var Translator = require('../translator/translator');
-var translator = new Translator();
-
-module.exports = function(difficulty, language) {
-    if (!this.dictionary)
-        this.dictionary = {
-            chinese: {},
-            arabic: {},
-            portugal: {},
-            russian: {},
-            spanish: {},
-            turkish: {}
-        };
-
-    if (!this.dictionary[language][difficulty])
-        this.dictionary[language][difficulty] = langs[language][difficulty].map(function (word) {
-            return { original: word.original, translation: word.chinese, transcription: translator.getTranscription(word.original) };
-        });
-
-    return this.dictionary[language][difficulty];
-};
+//var models = require('../app/models');
+//var langs = {
+//    chinese: require('../chinese'),
+//    arabic: require('../arabic'),
+//    portugal: require('../portugal'),
+//    russian: require('../russian'),
+//    spanish: require('../spanish'),
+//    turkish: require('../turkish')
+//};
+//
+//var Translator = require('../translator/translator');
+//var translator = new Translator();
+//
+//module.exports = function(difficulty, language) {
+//    if (!this.dictionary)
+//        this.dictionary = {
+//            chinese: {},
+//            arabic: {},
+//            portugal: {},
+//            russian: {},
+//            spanish: {},
+//            turkish: {}
+//        };
+//
+//    if (!this.dictionary[language][difficulty])
+//        this.dictionary[language][difficulty] = langs[language][difficulty].map(function (word) {
+//            return { original: word.original, translation: word.chinese, transcription: translator.getTranscription(word.original) };
+//        });
+//
+//    return this.dictionary[language][difficulty];
+//};
